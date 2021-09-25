d4b4a316deed537bdaed5ec23499bbc57f6a79fe
Pilation <bogdanleontovich@gmail.com>
2021-09-25 19:24:20 +0300

    7:24 25.09

--- js/script.js
+++ js/script.js
@@ -37,6 +37,30 @@ $(document).ready(function () {
         // appendArrows: $(".arws"),
         // переместить стрелки в див
         // appendDots: $(".arws"),
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ],
     });
     $('.header__burger').click(function (event) {
         $('.header__burger,.header-menu').toggleClass('active');
@@ -80,6 +104,30 @@ $(document).ready(function () {
         appendArrows: $(".category-quote__arrows"),
         // переместить стрелки в див
         // appendDots: $(".arws"),
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ],
     });
     $('a[href^="#link"]').each(function () {
         var oldUrl = $(this).attr("href"); // Get current url
